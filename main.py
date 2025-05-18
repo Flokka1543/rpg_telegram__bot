6a57cb48e345b49489cd38564a820f2eb4fca69e
flokka <daniel.em.05@mail.ru>
2025-05-18 14:39:38 +0300

    был добавлен участок кода,с проверкой на четность чисел

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import random
 points = {"comp":0,"player":0}
+
+def is_odd(number: int):
+    if number % 2 == 0:
+        return False
+    else:
+        return True
+
 while True:
     a = random.randint(10,100)
+    if is_odd(a):
+        continue
     b = random.randint(10, 100)
+    if is_odd(b):
+        continue
     op = random.choice(("+","-"))
     if op == "+":
         ans = a + b
